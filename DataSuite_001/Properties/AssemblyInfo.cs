6f8fbe3da91e4b30264827dba50438b856d54508
Vineet Reddy <vineetur@gmail.com>
2018-10-16 21:23:06 +1100

    Commit to Remote Repo

---
+++ DataSuite_001/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataSuite_001")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataSuite_001")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64bc989e-c030-4ef5-b273-40a2956f79bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DataSuite_001/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TestStack.White;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White.UIItems.WindowItems;
+
+namespace DataSuite_001
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private TestStack.White.Application application;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string app_path = @"D:\RESOURCES\DataSuite\1245\Console\DataSuite.Administration.Console.WinClient.exe";
+            application = Application.Launch(app_path); 
+            Window mainWindow = application.GetWindow("DataSuite Console");
+            Console.WriteLine(mainWindow.Title);
+            mainWindow.WaitWhileBusy();
+            mainWindow.Close();
+        }
+    }
+}
